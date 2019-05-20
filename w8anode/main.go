1f630b4e5e6b68c99e15c5eaf5fd00b598e9048a
mntky <djminatoo@gmail.com>
2019-05-21 00:25:37 +0900

    [refactoring]change w8anode switch process

--- w8anode/main.go
+++ w8anode/main.go
@@ -20,7 +20,7 @@ var respstruct *api.ContainerState
 type Act struct {
         Action string
         Option string
-        Conta  string
+        Args  string
 }
 
 func main() {
@@ -56,35 +56,62 @@ func handleact(data net.Conn) {
 	if err := json.Unmarshal(jsonByte, act); err != nil {
 		fmt.Println(err)
 	}
-	fmt.Printf("action: %v \n",act.Action)
-	fmt.Printf("options: %v \n",act.Option)
-	fmt.Printf("container: %v \n",act.Conta)
+	fmt.Printf("action: %v \n", act.Action)
+	fmt.Printf("options: %v \n", act.Option)
+	fmt.Printf("container: %v \n", act.Args)
 
 	lxdconn := lxdpkg.Connect()
 	switch act.Action {
 		case "status":
-			resp := lxdpkg.Status(act.Conta, lxdconn)
-			respstruct = resp
-			fmt.Println(respstruct.Status)
-			sock.Send(respstruct.Status)
+			switch act.Option {
+				case "pod":
+					if act.Args == "" {
+						resp := lxdpkg.Status(act.Args, lxdconn)
+						respstruct = resp
+						sock.Send(respstruct.Status)
+					}else {
+						resp := lxdpkg.Status(act.Args, lxdconn)
+						respstruct = resp
+						sock.Send(respstruct.Status)
+					}
+					return
+
+				case "node":
+					if act.Args == "" {
+						//return node status
+						sock.Send("all node resp")
+					}else {
+						//return receive node status
+						sock.Send("receive node resp")
+					}
+					return
+
+				default:
+					fmt.Println("w8a status (container, pod, node)")
+			}
+
 		case "create":
-			resp, err := lxdpkg.Create(act.Conta, lxdconn)
+			//w8a create "container-image" "container-name"
+			resp, err := lxdpkg.Create(act.Option, act.Args, lxdconn)
 			if err != nil {
 				fmt.Println(err)
 			}
 			sock.Send(resp)
+
 		case "delete":
-			resp, err := lxdpkg.Delete(act.Conta, lxdconn)
+			resp, err := lxdpkg.Delete(act.Args, lxdconn)
 			if err != nil {
 				fmt.Println(err)
 			}
 			sock.Send(resp)
+
 		case "start":
-			resp, err := lxdpkg.Start(act.Conta, lxdconn)
+			resp, err := lxdpkg.Start(act.Args, lxdconn)
 			if err != nil {
 				fmt.Println(err)
 			}
 			sock.Send(resp)
+
 		default:
 			fmt.Println("nothing command")
 	}
