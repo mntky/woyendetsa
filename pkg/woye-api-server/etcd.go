999781e2cd0d32e6d04fbf47c005e2994f9e8744
mntky <djminatoo@gmail.com>
2019-12-03 11:08:18 +0900

    add etcd pkg

---
+++ pkg/woye-api-server/etcd.go
@@ -0,0 +1,32 @@
+package pkg
+
+import (
+	"os"
+	"fmt"
+	"time"
+	"context"
+
+	"go.etcd.io/etcd/clientv3"
+)
+
+
+func main() {
+	cli, err := clientv3.New(clientv3.Config{
+		Endpoints:	[]string{"http://localhost:2379"},
+		DialTimeout: 5 * time.Second,
+	})
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer cli.Close()
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	resp, err := cli.Get(ctx, "/pod")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	fmt.Printf("%q\n",resp.Kvs)
+	fmt.Printf("%T\n", cancel)
+}
+
